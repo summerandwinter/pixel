73b69f6fd9a530190a79527fc18102f1749c7b51
winter <xiaopihailaotou@gmail.com>
2018-02-12 17:56:08 +0800

    缩放功能没达到预期效果，还要继续完善

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -5,6 +5,8 @@ Page({
   isMoving:false,
   x: 0,
   y: 0,
+  w: 0,
+  h: 0,
   tanslateX: 0,
   tanslateY: 0,
   centerX: 0,
@@ -26,13 +28,12 @@ Page({
       this.isZooming = true;
       var xMove = e.touches[1].x - e.touches[0].x;
       var yMove = e.touches[1].y - e.touches[0].y;
-      this.centerX = (e.touches[1].x + e.touches[0].x) / 2 + this.tanslateX;
-      this.centerY = (e.touches[1].y + e.touches[0].y) / 2 + this.tanslateY;
-      console.log("touch0:" + "(" + e.touches[0].x + "," + e.touches[0].y + ")");
-      console.log("touch1:" + "(" + e.touches[1].x + "," + e.touches[1].y + ")");
-      console.log("center:" + "(" + this.centerX + "," + this.centerY + ")");
+      this.centerX = (e.touches[1].x + e.touches[0].x) / 2;
+      this.centerY = (e.touches[1].y + e.touches[0].y) / 2;
       var distance = Math.sqrt(xMove * xMove + yMove * yMove) ;
       this.distance = distance;
+      this.w = Math.abs(xMove);
+      this.h = Math.abs(yMove);
       this.draw();
     }
   },
@@ -57,19 +58,13 @@ Page({
         var yMove = e.touches[1].y - e.touches[0].y;
         var distance = Math.sqrt(xMove * xMove + yMove * yMove);
 
-        var distanceDiff = distance / this.distance;
-        var scale = this.scale * distanceDiff;
-        var centerX = (e.touches[1].x + e.touches[0].x) / 2 + this.tanslateX;
-        var centerY = (e.touches[1].y + e.touches[0].y) / 2 + this.tanslateY;
-        console.log("touch0:" + "(" + e.touches[0].x + "," + e.touches[0].y + ")");
-        console.log("touch1:" + "(" + e.touches[1].x + "," + e.touches[1].y + ")");
-        console.log("center:" + "(" + centerX + "," + centerY + ")");
-        this.tanslateX -= (this.centerX * distanceDiff - this.centerX);
-        this.tanslateY -= (this.centerY * distanceDiff - this.centerY)
-
-        console.log("scale:" + scale);
-        console.log("tanslateX:" + this.tanslateX);
-        console.log("tanslateY:" + this.tanslateY);
+        var zoom = distance / this.distance;
+        var scale = this.scale * zoom;
+        centerX
+        var centerX = (e.touches[1].x + e.touches[0].x) / 2 ;
+        var centerY = (e.touches[1].y + e.touches[0].y) / 2;
+        this.tanslateX -= ((this.centerX * zoom - this.centerX) - (centerX - this.centerX));
+        this.tanslateY -= ((this.centerY * zoom - this.centerY) - (centerY - this.centerY));
         this.distance = distance;
         this.scale = scale;
         this.centerX = centerX;
@@ -92,15 +87,36 @@ Page({
   draw: function(){
     this.context.scale(this.scale, this.scale);
     this.context.translate(this.tanslateX , this.tanslateY );
+    var step = 50;
+    var x = this.tanslateX;
+    var y = this.tanslateY ;
+    for(var i = 0; i< 10; i++){
+      this.context.moveTo(x, y);
+      this.context.lineTo(x, y + 500);
+      
+      x+= step;
+
+    }
+
+    x = this.tanslateX;
+    y = this.tanslateY;
+    for (var i = 0; i < 10; i++) {
+      this.context.moveTo(x, y);
+      this.context.lineTo(x + 500, y );
+     
+      y += step;
+
+    }
+    this.context.stroke()
     // Clear screen to white.
-    this.context.setFillStyle("green");
-    this.context.fillRect(0, 0, this.width, this.height);
+    //this.context.setFillStyle("green");
+    //this.context.fillRect(0, 0, this.width, this.height);
     // Draw the black square.
-    this.context.setFillStyle("black");
-    this.context.fillRect(110, 202, 100, 100);  
+    //this.context.setFillStyle("black");
+    //this.context.fillRect(110, 202, 100, 100);  
     //this.context.save()
     this.context.beginPath()
-    this.context.arc(this.centerX * this.scale + this.tanslateX, this.centerY * this.scale + this.tanslateX, 2, 0, 2 * Math.PI)
+    this.context.arc(this.centerX + this.tanslateX, this.centerY + this.tanslateX, 2, 0, 2 * Math.PI)
     this.context.setFillStyle('lightgreen')
     this.context.fill()
     this.context.draw();
